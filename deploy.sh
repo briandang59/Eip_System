418e46f3457c63f3b3bf4ac5d94b7211d81f5641
unknown <cliowjay@gmail.com>
2025-07-03 10:28:10 +0700

    infra: blue/green deploy

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ sudo docker compose up -d --no-deps --build $INACTIVE_COLOR
 
 # 等待新容器健康檢查通過
 echo "等待 $INACTIVE_COLOR 服務健康..."
-while [ "$(docker inspect -f {{.State.Health.Status}} eip-ui-${INACTIVE_COLOR})" != "healthy" ]; do
+while [ "$(sudo docker inspect -f {{.State.Health.Status}} eip-ui-v2-${INACTIVE_COLOR})" != "healthy" ]; do
     echo -n "."
     sleep 3
 done
