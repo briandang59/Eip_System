93e52a4bd21b5723a51b20b138fc2fd9f3199619
unknown <cliowjay@gmail.com>
2025-07-03 10:12:12 +0700

    infra: blue/green deploy

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ echo "將要部署到: $INACTIVE_COLOR"
 
 # 建立並啟動閒置容器
 echo "正在建立新版本 $INACTIVE_COLOR..."
-docker compose up -d --no-deps --build $INACTIVE_COLOR
+sudo docker compose up -d --no-deps --build $INACTIVE_COLOR
 
 # 等待新容器健康檢查通過
 echo "等待 $INACTIVE_COLOR 服務健康..."
@@ -42,11 +42,11 @@ echo "正在切換流量至 $INACTIVE_COLOR..."
 sed -i "s/proxy_pass http:\/\/$ACTIVE_UPSTREAM;/proxy_pass http:\/\/${INACTIVE_COLOR}_upstream;/" "$NGINX_CONFIG_FILE"
 
 # 重新載入 Nginx 設定
-docker compose exec nginx nginx -s reload
+sudo docker compose exec nginx nginx -s reload
 echo "流量已成功切換！"
 
 # 停止舊的容器 (可選，也可以保留作為快速回滾)
 echo "正在停止舊的 $ACTIVE_COLOR 服務..."
-docker compose stop $ACTIVE_COLOR
+sudo docker compose stop $ACTIVE_COLOR
 
 echo "部署完成！"
