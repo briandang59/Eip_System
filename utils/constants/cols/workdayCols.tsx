7f4124a8ddf1e8d179d2d28879be825299677f42
Dang Viet Quang <dvquang5902@gmail.com>
2025-08-26 17:25:28 +0700

    fix: logic hide t1 and t2

--- utils/constants/cols/workdayCols.tsx
+++ utils/constants/cols/workdayCols.tsx
@@ -167,7 +167,13 @@ export const useWorkdayCols = ({
                 const time1 = record?.details[0]?.workday?.T1?.time;
                 const time2 = record?.details[0]?.workday?.T2?.time;
                 const face_t1 = record?.details[0]?.workday?.T1?.face_photo;
-                const leave_hours = record?.details[0]?.workday?.leave_hours?.B >= 8;
+                const leave_hours =
+                    record?.details[0]?.workday?.leave_hours?.A +
+                        record?.details[0]?.workday?.leave_hours?.B +
+                        record?.details[0]?.workday?.leave_hours?.C +
+                        record?.details[0]?.workday?.leave_hours?.DB +
+                        record?.details[0]?.workday?.leave_hours?.CV >=
+                    8;
 
                 if (leave_hours) return null;
 
@@ -226,7 +232,13 @@ export const useWorkdayCols = ({
                 const time1 = record?.details[0]?.workday?.T1?.time;
                 const time2 = record?.details[0]?.workday?.T2?.time;
                 const face_t2 = record?.details[0]?.attendance[0]?.T2.face_photo;
-                const leave_hours = record?.details[0]?.workday?.leave_hours?.B >= 8;
+                const leave_hours =
+                    record?.details[0]?.workday?.leave_hours?.A +
+                        record?.details[0]?.workday?.leave_hours?.B +
+                        record?.details[0]?.workday?.leave_hours?.C +
+                        record?.details[0]?.workday?.leave_hours?.DB +
+                        record?.details[0]?.workday?.leave_hours?.CV >=
+                    8;
 
                 if (leave_hours) return null;
 
